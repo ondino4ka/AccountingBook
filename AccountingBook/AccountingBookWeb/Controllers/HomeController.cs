a1fcd1ce31fc84c263ef0cc4b788c9860bcf028b
ondino4ka <kostik.cirotkin@yandex.ru>
2017-11-16 01:19:07 +0300

    AB - [0.2] - Added and Configured log4net for AccountingBookWeb

--- AccountingBook/AccountingBookWeb/Controllers/HomeController.cs
+++ AccountingBook/AccountingBookWeb/Controllers/HomeController.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using log4net;
 
 namespace AccountingBookWeb.Controllers
 {
     public class HomeController : Controller
     {
+        private static log4net.ILog Log = LogManager.GetLogger("HomeController"); 
+
         // GET: Home
         public ActionResult Index()
         {
+            Log.Debug("Test log");
             return View();
         }
     }
