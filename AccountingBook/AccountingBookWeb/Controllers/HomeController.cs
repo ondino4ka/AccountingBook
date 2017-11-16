0ba379e27752b2bc12f0cb1403ffe5d827f208ba
ondino4ka <kostik.cirotkin@yandex.ru>
2017-11-16 14:26:15 +0300

    AB - [0.2] - Added StructureMap package for AccountingBookWeb and Added Class Library project AccountingBookBL

--- AccountingBook/AccountingBookWeb/Controllers/HomeController.cs
+++ AccountingBook/AccountingBookWeb/Controllers/HomeController.cs
@@ -4,18 +4,32 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using log4net;
+using AccountingBookBL;
 
 namespace AccountingBookWeb.Controllers
 {
     public class HomeController : Controller
     {
-        private static log4net.ILog Log = LogManager.GetLogger("HomeController"); 
+        private static log4net.ILog Log = LogManager.GetLogger("HomeController");
 
+        private readonly IDependency dependency;
+        public HomeController(IDependency dependency)
+        {
+            this.dependency = dependency;
+        }
         // GET: Home
         public ActionResult Index()
         {
-            Log.Debug("Test log");
-            return View();
+            try
+            {
+                Log.Debug("Test log");
+                throw new Exception("Test Exception");
+            }
+            catch (Exception exception)
+            {
+                Log.Error(exception.Message);
+            }
+            return Content(dependency.GetMessages());
+            }
         }
     }
-}
